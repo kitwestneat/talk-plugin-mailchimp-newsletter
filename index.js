25385a2056f32e9354dcd5b0676cae26b6768632
Kit Westneat <kit.westneat@gmail.com>
2018-02-20 17:08:45 -0500

    beta version

--- index.js
+++ index.js
@@ -3,21 +3,52 @@ const { USERS_NEW } = require('services/events/constants');
 const debug = require('debug')('talk:plugin:mailchimp-newsletter');
 const Mailchimp = require('mailchimp-api-v3')
 
-if (!process.env.TALK_MAILCHIMP_API_KEY || !process.env.TALK_MAILCHIMP_LIST_ID)
-  throw new Error('Mailchimp plugin requires TALK_MAILCHIMP_API_KEY and TALK_MAILCHIMP_LIST_ID env parameters');
-
-const mailchimp = new Mailchimp(process.env.TALK_MAILCHIMP_API_KEY);
-
 function subUser(email_address) {
+  if (!mailchimp) {
+    console.warn('Mailchimp plugin requires TALK_MAILCHIMP_API_KEY and TALK_MAILCHIMP_LIST_ID env parameters');
+  }
+
   // 'subscribed' (single opt-in) or 'pending' (double opt-in)
   let status = process.env.TALK_MAILCHIMP_USER_STATUS || 'pending';
 
-  mailchimp.post(`/lists/${process.env.TALK_MAILCHIMP_LIST_ID}/members`, {
+  debug(`adding ${email_address} with status ${status}`);
+
+  return mailchimp.post(`/lists/${process.env.TALK_MAILCHIMP_LIST_ID}/members`, {
     email_address,
     status,
   });
 }
 
+let mailchimp;
+
+if (!process.env.TALK_MAILCHIMP_API_KEY || !process.env.TALK_MAILCHIMP_LIST_ID) {
+  console.warn('Mailchimp plugin requires TALK_MAILCHIMP_API_KEY and TALK_MAILCHIMP_LIST_ID env parameters');
+} else {
+  mailchimp = new Mailchimp(process.env.TALK_MAILCHIMP_API_KEY);
+}
+
 events.on(USERS_NEW, async user => {
-  debug(`got new user event: ` + JSON.stringify(user));
+  if (user.profiles.length == 0) {
+    debug(`cannot determine email address for user: ` + JSON.stringify(user));
+    return;
+  }
+
+  if (user.profiles.length > 1) {
+    debug(`new user has more than one user profile: ` + JSON.stringify(user));
+  }
+
+  let { id, provider, metadata } = user.profiles[0];
+  let email;
+  if (provider == 'local') {
+    email = id;
+  } else if (metadata && metadata.email) {
+    email = metadata.email;
+  } else {
+    debug(`cannot determine email address for user: ` + JSON.stringify(user));
+    return;
+  }
+
+  await subUser(email).catch(debug);
+
+  debug(`added ${email} to newsletter`);
 });
